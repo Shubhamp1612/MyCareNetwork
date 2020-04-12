4b1c0242343cf6899ac2f60391cebe6bdf0f7e0f
shubhampatil <shubham.patil@sjsu.edu>
2020-04-12 04:44:50 -0700

    Home Page activity

--- app/src/main/java/com/example/mycarenetwork/MainActivity.java
+++ app/src/main/java/com/example/mycarenetwork/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.mycarenetwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowTitleEnabled(false); //Hide Action Bar title text
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflator = getMenuInflater();
+        inflator.inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==R.id.Logout){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent) ;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
